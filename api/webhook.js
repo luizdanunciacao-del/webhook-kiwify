a2c4aa24eb5c83649b4b77249ca6bef4d6fa6674
luizdanunciacao-del <luiz.danunciacao@gmail.com>
2026-06-10 13:17:41 -0300

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+
+    if (req.method !== 'POST') {
+        return res.status(405).json({
+            error: 'Método não permitido'
+        });
+    }
+
+    console.log(req.body);
+
+    return res.status(200).json({
+        success: true
+    });
+
+}
